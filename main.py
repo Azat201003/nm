d93e7d5ed483fadff037f6e9ab0a72433c7d6a3c
Azat201003 <101720508+Azat201003@users.noreply.github.com>
2024-12-15 20:32:41 +0300

    notes-manager

---
+++ main.py
@@ -0,0 +1,138 @@
+import json
+import sys
+from abc import (abstractmethod, ABC)
+from datetime import datetime
+
+statuses = ['todo', 'in-progress', 'done']
+
+class Command(ABC):
+    def __init__(self, handler: 'Handler'):
+        self.handler = handler
+
+    @abstractmethod
+    def __call__(self, *args, **kwds):
+        pass
+
+    @property
+    @abstractmethod
+    def title(self):
+        pass
+
+class ListCommand(Command):
+    title = 'list'
+    def __call__(self, status=None, *args):
+        if status is None:
+            self.print_notes(self.handler.data)
+        else:
+            if status not in statuses:
+                print('status should be todo, in-progress or done')
+                return
+            self.print_notes(list([note for note in self.handler.data if note['status'] == status]))
+    
+    def print_notes(self, notes:list, sort_by='id'):
+        for note in sorted(notes, key=lambda note: note[sort_by]):
+            print(f'[{note['id']}] {note['status']} "{note['content']}" created at {note['createdAt']} updated at {note['updatedAt']}')
+
+class AddCommand(Command):
+    title = 'add'
+    def __call__(self, content, *args):
+        print(f"Task added successfully (ID: {self.handler.add_note(content)})")
+
+class UpdateCommand(Command):
+    title = 'update'
+    def __call__(self, id, content, *args):
+        self.handler.change_note(int(id), content)
+
+class MarkInProgress(Command):
+    title = 'mark-in-progress'
+    def __call__(self, id, *args):
+        self.handler.mark_in_progress(int(id))
+
+class MarkDone(Command):
+    title = 'mark-done'
+    def __call__(self, id, *args):
+        self.handler.mark_done(int(id))
+
+class Handler:
+    def __init__(self):
+        with open('data.json', 'r+') as file:
+            self.data = json.load(file)
+    
+    def add_note(self, content):
+        id = len(self.data)
+        self.data.append({
+            'id': id,
+            'status': 'todo',
+            'content': content,
+            'createdAt': datetime.now().strftime('%H:%M:%S'),
+            'updatedAt': datetime.now().strftime('%H:%M:%S'),
+        })
+        with open('data.json', 'w') as file:
+            json.dump(self.data, file)
+        
+        return id
+    
+    def change_note(self, id, content):
+        self.data[id]['content'] = content
+        self.data[id]['updatedAt'] = datetime.now().strftime('%H:%M:%S')
+        with open('data.json', 'w') as file:
+            json.dump(self.data, file)
+        
+    def mark_in_progress(self, id:int):
+        self.data[id]['status'] = 'in-progress'
+        self.data[id]['updatedAt'] = datetime.now().strftime('%H:%M:%S')
+        with open('data.json', 'w') as file:
+            json.dump(self.data, file)
+
+    def mark_done(self, id:int):
+        self.data[id]['status'] = 'done'
+        self.data[id]['updatedAt'] = datetime.now().strftime('%H:%M:%S')
+        with open('data.json', 'w') as file:
+            json.dump(self.data, file)
+
+class Invoker:
+    def __init__(self):
+        self.handler = Handler()
+        self.commands = [
+            ListCommand(self.handler),
+            AddCommand(self.handler),
+            UpdateCommand(self.handler),
+            MarkDone(self.handler),
+            MarkInProgress(self.handler),
+        ]
+        self.commands_by_name = {}
+        for command in self.commands:
+            self.commands_by_name[command.title] = command
+    
+    def __call__(self, command, *args, **kwds):
+        self.commands_by_name[command](*args)
+
+try:
+    with open('data.json', 'r+') as file:
+        if file.read() == '':
+            file.write('[]')
+except:
+    with open('data.json', 'w+') as file:
+        file.write('[]')
+
+invoker = Invoker()
+
+inp = sys.argv[1:]
+command = inp[0]
+if len(inp) > 1:
+    args = inp[1:]
+    invoker(command, *args)
+else:
+    invoker(command)
+
+
+
+'''
+        print(args)
+структура json'а:
+
+[
+    {id: 0, content: 'afsdafdsdfas', status: 'done', createdAt: 01.02.2024, updatedAt: 02.02.2024}
+]
+
+'''
